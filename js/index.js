b90cd52fcbde4843140d0593f93abea452d7f023
Martseniuk Alina <martseniuk.av@gmail.com>
2024-12-31 19:53:32 +0200

    add

--- js/index.js
+++ js/index.js
@@ -6,18 +6,34 @@
 // “поповнити рахунок” та отримати ‘готівку’ відповідно. Після проведення операції виводити
 // повідомлення про залишок на рахунку.
 
+const bankAccount = {
+  ownerName: 'Олексій',
+  accountNumber: 'UA1234567890',
+  balance: 1000,
+  deposit(amount) {
+    if (amount > 0) {
+      this.balance += amount;
+      console.log(`Ви внесли ${amount} грн. Залишок на рахунку: ${this.balance} грн.`);
+    } else {
+      console.log('Сума для внесення повинна бути більше 0.');
+    }
+  },
+  withdraw(amount) {
+    if (amount > 0 && amount <= this.balance) {
+      this.balance -= amount;
+      console.log(`Ви зняли ${amount} грн. Залишок на рахунку: ${this.balance} грн.`);
+    } else if (amount > this.balance) {
+      console.log('Недостатньо коштів на рахунку.');
+    } else {
+      console.log('Сума для зняття повинна бути більше 0.');
+    }
+  }
+};
 
-// const bankAccount = {
-//     ownerName: NoName,
-//     accountNumber: 5,
-//     balance: 6,
-//     deposit() {
-//     console.log("deposit");
-//     },
-//     withdraw() {
-//     console.log("withdraw");
-//     },
-// }
+bankAccount.deposit(500);
+bankAccount.withdraw(300);
+bankAccount.withdraw(2000);
+bankAccount.deposit(-100);
 
 //*\\\\\\\\\\\\\\\\\\\\\\завдання 2\\\\\\\\\\\\\\\\\\\\\\\\
 //Створіть об'єкт "weather" з властивостями "temperature", "humidity", "windSpeed".
@@ -26,27 +42,47 @@
 //  інпуту на сторінці. Якщо метод повернув "true" вивести повідомлення “температура нижче 0 градусів
 // Цельсія” і навпаки
 
-// const weather = {
-//     temperature: 8,
-//     humidity: 99,
-//     windSpeed: 77
-// }
+const weather = {
+    temperature: 8,
+    humidity: 99,
+    windSpeed: 77,
+    compare(temperature) {
+        if (temperature > 0) {
+            console.log(true);
+            
+        } else {
+            console.log(false);
+            
+        }
+    }
+}
 
-
-
+weather.compare(77)
+console.log(weather);
 
 
 //*\\\\\\\\\\\\\\\\\\\\\\завдання 3\\\\\\\\\\\\\\\\\\\\\\\\
 //Створіть об’єкт "user", який буде мати властивості "name", "email", "password".
-// Додайте метод "login", який буде перевіряти правильність введеного email та password.
+ //Додайте метод "login", який буде перевіряти правильність введеного email та password.
 // Використайте інпути для запису значень властивостей в об’єкт
 
-// const user = {
-//     name: 6,
-//     email: 6,
-//     password: 6
-// }
+const user = {
+  name: 'Іван',
+  email: 'ivan@example.com',
+  password: 'securepassword',
+  login(inputEmail, inputPassword) {
+    if (inputEmail === this.email && inputPassword === this.password) {
+      console.log('Вхід успішний');
+      return true;
+    } else {
+      console.log('Невірний email або пароль');
+      return false;
+    }
+  }
+};
 
+user.login('ivan@example.com', 'securepassword'); // Вхід успішний
+user.login('ivan@example.com', 'wrongpassword');  // Невірний email або пароль
 
 
 
@@ -57,13 +93,21 @@
 // Якщо метод повернув "true" то змінити колір тексту поля title на зелений.
 
 
-// const movie = {
-//     title: 6,
-//     director: 9,
-//     year: 0,
-//     rating: 1
-// }
-
+const movie = {
+    title: 6,
+    director: 9,
+    year: 0,
+    rating: 1,
+    method(rating) {
+        if (rating > 8) {
+            console.log('true');
+            
+        } else {
+            console.log('false');
+        }
+    }
+}
+movie.method(7);
 
 
 
